83cb1f9b1cbdf1d04636d2689293d184cec82702
chenwenhao <zerochan1220@163.com>
2019-03-08 10:08:54 +0800

    Initial Commit

---
+++ WHXMLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHXMLDemo
+//
+//  Created by zero on 2019/3/8.
+//  Copyright © 2019 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHXMLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHXMLDemo
+//
+//  Created by zero on 2019/3/8.
+//  Copyright © 2019 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
